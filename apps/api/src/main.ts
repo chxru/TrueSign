b5149f35060a1257dc967b40efb9e48cbadd24af
Charuka Samarakoon <charuka@protonmail.com>
2023-03-02 14:30:40 +0530

    Upload images to s3

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,25 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+import fileUpload from 'express-fileupload';
+
+// routes
+import imagesRouter from './controllers/images.controller';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// middleware
+app.use(fileUpload());
 
-app.get('/api', (req, res) => {
+// routes
+app.use('/image', imagesRouter);
+
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
+
 server.on('error', console.error);
