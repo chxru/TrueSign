751ce5f5bbc33c884c19633b28e48840f3977946
Charuka Samarakoon <charuka@protonmail.com>
2023-04-01 09:07:57 +0530

    Integrate clerk.dev

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,16 +1,41 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from '@clerk/nextjs';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const publicPages: string[] = ['/sign-in', '/sign-up'];
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
   return (
     <>
       <Head>
         <title>Welcome to web!</title>
       </Head>
+
       <main className="app">
         <ChakraProvider>
-          <Component {...pageProps} />
+          <ClerkProvider {...pageProps}>
+            {!publicPages.includes(pathname) ? (
+              <Component {...pageProps} />
+            ) : (
+              <>
+                <SignedIn>
+                  <Component {...pageProps} />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            )}
+          </ClerkProvider>
         </ChakraProvider>
       </main>
     </>
