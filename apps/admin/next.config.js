5f0b96d9b26b7fce877c67d2431f41f9bdaa4466
Charuka Samarakoon <charuka@protonmail.com>
2023-04-08 14:22:25 +0530

    Redirect /api requests to actual API

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3333/:path*',
+      }
+    ]
+  }
 };
 
 module.exports = withNx(nextConfig);
