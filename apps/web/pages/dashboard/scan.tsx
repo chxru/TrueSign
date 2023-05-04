0549a2fc14b796b318d38dc1202597b6cf2c04c3
Charuka Samarakoon <charuka@protonmail.com>
2023-05-04 07:41:56 +0530

    Downloadable pdf

--- apps/web/pages/dashboard/scan.tsx
+++ apps/web/pages/dashboard/scan.tsx
@@ -5,7 +5,16 @@ import {
   Grid,
   GridItem,
   Image as ChakraImage,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
   Stack,
+  useDisclosure,
 } from '@chakra-ui/react';
 import cv from 'opencv-ts';
 import { useState, useEffect, useRef } from 'react';
@@ -173,6 +182,10 @@ const Toolbar = () => {
   const addImage = useScannerStore((state) => state.addImage);
   const selectedImageId = useScannerStore((state) => state.selectedImageId);
 
+  const [confirmPressed, setConfirmPressed] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const handleUpload = (files: FileList) => {
     Array.from(files).forEach((file) => {
       const data: Omit<IUploadFile, 'border'> = {
@@ -202,41 +215,118 @@ const Toolbar = () => {
     const height = canvas.height;
     formData.append('resolution', `${width}x${height}`);
 
-    const response = await fetch('http://localhost:5000/upload', {
+    await fetch('http://localhost:5000/upload', {
       method: 'POST',
       body: formData,
     });
 
-    const data = await response.json();
-    console.log(data);
+    setConfirmPressed(true);
+  };
+
+  const getDownloadLink = async (filter: string) => {
+    const formData = new FormData();
+    formData.append('filter', filter);
+
+    const res = await fetch('http://localhost:5000/download', {
+      method: 'POST',
+      body: formData,
+    });
+
+    setProcessing(false);
+
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = url;
+    link.download = 'doc.pdf';
+
+    document.body.appendChild(link);
+    link.click();
   };
 
   return (
-    <Stack direction={'row'} gap={4}>
-      <input
-        ref={inputRef}
-        type="file"
-        name="image-upload"
-        multiple={true}
-        style={{ display: 'none' }}
-        onChange={(e) => {
-          handleUpload(e.target.files);
-        }}
-      />
-      <Button
-        colorScheme="blue"
-        variant="outline"
-        onClick={() => {
-          if (inputRef.current) {
-            inputRef.current.click();
-          }
-        }}
-      >
-        Upload Image
-      </Button>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
 
-      <Button onClick={onConfirmCorners}>Confirm Corners</Button>
-    </Stack>
+        <ModalContent>
+          <ModalHeader>Select a template</ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody>
+            <Select
+              placeholder="Select filter"
+              onChange={async (e) => {
+                setProcessing(true);
+                await getDownloadLink(e.target.value);
+              }}
+            >
+              <option value="grayscale">Grayscale</option>
+              <option value="threshold">Threshold</option>
+              <option value="sharpen">Sharpen</option>
+              <option value="clahe">Clahe</option>
+              <option value="enhanced">Enhanced</option>
+              <option value="adjusted">Adjusted</option>
+            </Select>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme={'teal'} isLoading={processing}>
+              Download
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Flex justify={'center'}>
+        <Stack direction={'row'} gap={4}>
+          <input
+            ref={inputRef}
+            type="file"
+            name="image-upload"
+            multiple={true}
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              handleUpload(e.target.files);
+            }}
+          />
+
+          <Button
+            colorScheme="teal"
+            onClick={() => {
+              if (inputRef.current) {
+                inputRef.current.click();
+              }
+            }}
+          >
+            Upload Image
+          </Button>
+
+          {images.length && (
+            <>
+              <Button
+                colorScheme={'teal'}
+                onClick={onConfirmCorners}
+                isDisabled={selectedImageId === undefined}
+              >
+                Confirm Corners
+              </Button>
+
+              <Button
+                colorScheme={'teal'}
+                onClick={onOpen}
+                isDisabled={!confirmPressed}
+              >
+                Get PDF
+              </Button>
+            </>
+          )}
+        </Stack>
+      </Flex>
+    </>
   );
 };
 
