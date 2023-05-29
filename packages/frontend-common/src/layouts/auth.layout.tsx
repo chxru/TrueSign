0b36de651d0204d30e8b4614965df6c0bbd1a983
Charuka Samarakoon <charuka@protonmail.com>
2023-05-29 15:06:48 +0530

    Generate jwt and update periodically

--- packages/frontend-common/src/layouts/auth.layout.tsx
+++ packages/frontend-common/src/layouts/auth.layout.tsx
@@ -1,6 +1,7 @@
-import { useUser } from '@clerk/nextjs';
+import { useUser, useClerk } from '@clerk/nextjs';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Fetcher } from '../axios';
 
 interface AuthLayoutProps {
   children: JSX.Element;
@@ -17,7 +18,35 @@ enum AuthState {
 
 export const AuthLayout = (props: AuthLayoutProps) => {
   const { user } = useUser();
+  const { session } = useClerk();
   const [state, setState] = useState<AuthState>(AuthState.LOADING);
+
+  const generateToken = async () => {
+    const token = await session?.getToken({ template: 'default' });
+    Fetcher.setToken(token || null);
+
+    if (token) {
+      try {
+        const payload = token.split('.')[1];
+        const decoded = Buffer.from(payload, 'base64').toString('utf-8');
+        const parsed = JSON.parse(decoded);
+
+        const exp = parsed['exp'] as number;
+        const now = new Date().getTime() / 1000;
+        const wait = (exp - now) * 1000;
+        generateJWTPeriodically(wait);
+      } catch (error) {
+        console.error('Failed to parse token');
+        console.error(error);
+      }
+    }
+  };
+
+  const generateJWTPeriodically = (wait: number) => {
+    return setTimeout(async () => {
+      await generateToken();
+    }, wait);
+  };
 
   useEffect(() => {
     if (!user) return;
@@ -25,6 +54,12 @@ export const AuthLayout = (props: AuthLayoutProps) => {
     const isStaff = !!user.publicMetadata['isStaff'];
     const isAdmin = !!user.publicMetadata['isSuperAdmin'];
     const isStudent = !!user.publicMetadata['isStudent'];
+
+    /**
+     * Kick off the token generation process immediately here
+     * and then periodically, based on expire time after that.
+     */
+    const timer = generateJWTPeriodically(0);
 
     if (props.allowStaff && isStaff) {
       setState(AuthState.ALLOWED);
@@ -42,6 +77,10 @@ export const AuthLayout = (props: AuthLayoutProps) => {
     }
 
     setState(AuthState.DENIED);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [user]);
 
   if (state === AuthState.LOADING) {
