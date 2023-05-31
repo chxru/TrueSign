7500f0937c453ef2def6e5e4fde1c6f0edf363c2
Charuka Samarakoon <charuka@protonmail.com>
2023-05-31 14:44:52 +0530

    Check cv is loaded periodically

--- apps/web/pages/dashboard/scan.tsx
+++ apps/web/pages/dashboard/scan.tsx
@@ -1,6 +1,6 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import cv from 'opencv-ts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Footer, Stage, Toolbar } from '../../components/scanner/';
 
 const UploadPage = () => {
@@ -8,6 +8,25 @@ const UploadPage = () => {
   cv.onRuntimeInitialized = () => {
     setCvLoaded(true);
   };
+
+  useEffect(() => {
+    /**
+     * In some cases, the cv.onRuntimeInitialized is not called but the cv is loaded.
+     * Therefore, running a timer to check if the cv is loaded.
+     */
+    if (cvLoaded) return;
+
+    const timer = setInterval(() => {
+      if (cv) {
+        clearInterval(timer);
+        setCvLoaded(true);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [cvLoaded]);
 
   if (!cvLoaded) {
     return <div>Loading...</div>;
